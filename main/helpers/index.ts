99ae457b4c5adedf7e4a6f99e7b2a8cc5a444e03
zhangyicheng95 <1239271010@qq.com>
2025-01-10 10:34:09 +0800

    去除缓存

--- main/helpers/index.ts
+++
@@ -1,5 +0,0 @@
-export { createWindow } from './createWindow'
-export { createTray } from './createTray';
-export { createMessageEvents } from './createEvents';
-export { updateHandle } from './createUpdate';
-export { createMenus } from './createMenus';
